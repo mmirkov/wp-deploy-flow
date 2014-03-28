ab573956e5957c1a015cb0e05d8e4b14e57e6165
Miroslav Mirkov <mirkov@gmail.com>
2014-03-28 13:58:20 -0400

    Deactivate W3 Total Cache after successful pull

--- lib/puller.php
+++ lib/puller.php
@@ -21,6 +21,10 @@ class WP_Deploy_Flow_Puller {
     $commands[]= array('rm dump.sql', true);
 
     $this->_commands_for_files( $commands );
+    
+    // Deactivate W3 Total cache after successful pull
+    $commands[]= array('wp plugin deactivate w3-total-cache', true);
+
     return $commands;
   }
 
